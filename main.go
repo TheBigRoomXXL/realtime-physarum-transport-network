7b723c3128db2b6592c47af6a8bb977351062c90
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-09-07 19:04:51 +0200

    adding diffusion to the trail

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	_ "image/png"
@@ -10,10 +9,15 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-const N = 50
+const N = 20
 const SCREEN_WIDTH = 1024
 const SCREEN_HEIGHT = 768
 const PARTICULE_SIZE = 5
+const PARTICULE_SPEED = PARTICULE_SIZE * 50
+const SENSOR_DISTANCE = PARTICULE_SPEED * 9
+const SENSOR_ANGLE = 45
+const DIFFUSION_SIZE = 2
+const TRAIL_STRENGTH = 255
 
 type Particule struct {
 	Pos rl.Vector2
@@ -21,15 +25,19 @@ type Particule struct {
 }
 
 func (p *Particule) Draw() {
-	direction := rl.Vector2{
-		X: p.Pos.X + p.Vel.X*PARTICULE_SIZE*2,
-		Y: p.Pos.Y + p.Vel.Y*PARTICULE_SIZE*2,
-	}
 	rl.DrawCircleV(p.Pos, PARTICULE_SIZE, rl.Blue)
-	rl.DrawLineV(p.Pos, direction, rl.Blue)
 }
 
 func (p *Particule) Move(grid *Grid) {
+	// sampleCenter := rl.Vector2{
+	// 	X: p.Pos.X + p.Vel.X,
+	// 	Y: p.Pos.Y + p.Vel.Y,
+	// }
+	// velLeft := rl.Vector2Rotate(p.Vel, SENSOR_ANGLE)
+	// sampleLeft := rl.Vector2{
+	// 	X: p.Pos.X + p.Vel.X,
+	// 	Y: p.Pos.Y + p.Vel.Y,
+	// }
 	p.Pos.X += p.Vel.X
 	p.Pos.Y += p.Vel.Y
 	grid.Add(p.Pos, 0.5)
@@ -62,11 +70,13 @@ func NewGrid(w, h int) *Grid {
 
 func (g *Grid) Add(pos rl.Vector2, a float32) {
 	rl.ImageDrawCircleV(
-		g.Image, pos, PARTICULE_SIZE, color.RGBA{255, 255, 255, 25},
+		g.Image, pos, PARTICULE_SIZE, color.RGBA{255, 255, 255, TRAIL_STRENGTH},
 	)
 }
 
 func (g *Grid) Draw() {
+	rl.ImageBlurGaussian(g.Image, 1)
+	rl.ImageColorTint(g.Image, color.RGBA{255, 255, 255, 254})
 	colors := rl.LoadImageColors(g.Image)
 	rl.UpdateTexture(g.Texture, colors)
 	rl.DrawTexture(g.Texture, 0, 0, color.RGBA{255, 255, 255, 255})
@@ -108,16 +118,15 @@ func main() {
 
 	// Rendering loop
 	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-		rl.ClearBackground(color.RGBA{22, 23, 31, 100})
-
 		for i := 0; i < len(particules); i++ {
 			particules[i].Move(grid)
-			particules[i].Draw()
 		}
+
+		rl.BeginDrawing()
+		rl.ClearBackground(color.RGBA{22, 23, 31, 255})
 		grid.Draw()
 
-		rl.DrawText(fmt.Sprint("FPS: ", rl.GetFPS()), 20, 20, 20, rl.LightGray)
+		// rl.DrawText(fmt.Sprint("FPS: ", rl.GetFPS()), 20, 20, 20, rl.LightGray)
 		rl.EndDrawing()
 	}
 }
